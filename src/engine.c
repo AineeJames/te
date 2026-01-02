67e9a272bb10d54e0287701e22b96b1a3bd2df47
James Wilcock <jameswilcock8@gmail.com>
2026-01-01 20:43:02 -0800

    added script watching and hot reloading

--- src/engine.c
+++ src/engine.c
@@ -11,6 +11,88 @@
 #include <raylib.h>
 #include <stdlib.h>
 
+#ifdef __linux__
+#include <fcntl.h>
+#include <sys/inotify.h>
+#endif
+
+void init_engine_lua_script(Engine *engine) {
+  const char *main_path = TextFormat("%s/main.lua", engine->game_path);
+  if (luaL_dofile(engine->L, main_path) != LUA_OK) {
+    slog(FATAL, "Failed to load main.lua: %s", lua_tostring(engine->L, -1));
+  }
+
+  call_load(engine->L);
+}
+
+#define EVENT_SIZE (sizeof(struct inotify_event))
+#define BUF_LEN (1024 * (EVENT_SIZE + 16))
+
+void add_lua_file_watch(Engine *engine, const char *path) {
+
+#ifdef __linux__
+  int inotify_err =
+      inotify_add_watch(engine->watch_handle, path,
+                        IN_ATTRIB | IN_MODIFY | IN_CREATE | IN_DELETE);
+  assert(inotify_err != -1 && "failed to add watch");
+#endif
+}
+
+void init_lua_file_watch(Engine *engine) {
+#ifdef __linux__
+  int inotifyFd = inotify_init(); /* Create inotify instance */
+  assert(inotifyFd != -1 && "inotify init failed");
+  int wd;
+  // TODO go through lua script path and add all lua files
+  int flags = fcntl(inotifyFd, F_GETFL, 0);
+  fcntl(inotifyFd, F_SETFL, flags | O_NONBLOCK);
+  engine->watch_handle = inotifyFd;
+  add_lua_file_watch(engine, TextFormat("%s/main.lua", engine->game_path));
+#endif
+}
+
+bool poll_lua_file_change(Engine *engine) {
+  char buffer[BUF_LEN] = {0};
+  assert(engine->watch_handle != 0);
+  bool change = false;
+
+#ifdef __linux__
+  int length = read(engine->watch_handle, buffer, BUF_LEN);
+
+  int i = 0;
+  while (i < length) {
+    struct inotify_event *event = (struct inotify_event *)&buffer[i];
+
+    printf("File event\n");
+    if (event->mask & IN_MODIFY) {
+      printf("File was modified.\n");
+      change = true;
+    }
+    if (event->mask & IN_CREATE) {
+      printf("File was created.\n");
+      change = true;
+    }
+    if (event->mask & IN_DELETE) {
+      printf("File was deleted.\n");
+      change = true;
+    }
+    if (event->mask & IN_ATTRIB) {
+      printf("File was changed\n");
+      change = true;
+    }
+    if (event->mask & IN_IGNORED) {
+      printf("File was removed from inotify adding back\n");
+      add_lua_file_watch(engine, TextFormat("%s/main.lua", engine->game_path));
+      change = true;
+    }
+
+    i += EVENT_SIZE + event->len;
+  }
+#endif
+
+  return change;
+}
+
 Engine *engine_init(const char *game_path) {
   Engine *engine = malloc(sizeof(Engine));
   assert(engine);
@@ -18,6 +100,8 @@ Engine *engine_init(const char *game_path) {
   engine->running = false;
   engine->exit_code = 0;
   engine->game_path = game_path;
+
+  init_lua_file_watch(engine);
 
   engine->L = luaL_newstate();
   assert(engine->L);
@@ -39,12 +123,14 @@ Engine *engine_init(const char *game_path) {
   engine->grid = grid_init(w, h);
   grid_fill(engine->grid, CELL_EMPTY);
 
+  init_engine_lua_script(engine);
+
+  /*
   const char *main_path = TextFormat("%s/main.lua", engine->game_path);
   if (luaL_dofile(engine->L, main_path) != LUA_OK) {
     slog(FATAL, "Failed to load main.lua: %s", lua_tostring(engine->L, -1));
   }
-
-  call_load(engine->L);
+  */
 
   engine->renderer = renderer_init(engine);
 
@@ -170,6 +256,10 @@ int engine_run(Engine *engine) {
 
   while (engine->running) {
     float dt = GetFrameTime();
+
+    if (poll_lua_file_change(engine)) {
+      init_engine_lua_script(engine);
+    }
 
     /* --- Input --- */
     if (IsKeyPressed(KEY_ESCAPE)) {
